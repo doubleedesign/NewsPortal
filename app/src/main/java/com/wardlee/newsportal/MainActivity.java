b2b01902fee8d9c3d48eb5a6c08e52d147e0657c
Leesa Ward <wardlee@deakin.edu.au>
2020-05-05 00:03:09 +1000

    Got API call working, articles displaying in MainActivity

--- app/src/main/java/com/wardlee/newsportal/MainActivity.java
+++ app/src/main/java/com/wardlee/newsportal/MainActivity.java
@@ -9,12 +9,37 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     // Tag for debugging
     private static final String TAG = "MainActivity";
+
+    // API URL to get articles from
+    private static final String JSON_URL = "https://newsapi.org/v2/top-headlines?country=au&apiKey=aee4fbcb084948f185beee555aabaf62";
+
+    // List to store articles from the API
+    private ArrayList<Article> articleList = new ArrayList<Article>();
+
+    // How many articles to show
+    private int articleQty = 1;
 
 
     /**
@@ -26,7 +51,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Lookup the recyclerview in the activity layout
+        // Request the latest articles from NewsAPI.org
+        if(isInternetAvailable()) {
+            this.loadArticles();
+        } else {
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Network error");
+            alertDialog.setMessage("Please check your network connection so you can see the latest news from NewsAPI.org and access links in this application.");
+            alertDialog.show();
+        }
+
+        // Lookup the news outlet list recyclerview in the activity layout
         RecyclerView rvOutlets = findViewById(R.id.rv_outlets);
 
         // Initialize items
@@ -43,17 +78,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Add a basic divider between the items
         rvOutlets.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-
-        // Request the latest articles from NewsAPI.org
-        String feedURL = "https://newsapi.org/v2/top-headlines?country=au&pageSize=1&apiKey=aee4fbcb084948f185beee555aabaf62";
-        if(isInternetAvailable()) {
-           // TODO
-        } else {
-            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-            alertDialog.setTitle("Network error");
-            alertDialog.setMessage("Please check your network connection so you can see the latest news from NewsAPI.org and access links in this application.");
-            alertDialog.show();
-        }
     }
 
 
@@ -66,6 +90,88 @@ public class MainActivity extends AppCompatActivity {
     public boolean isInternetAvailable() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+
+
+    /**
+     * Method to load articles from NewsAPI.org using Volley
+     * Based on tutorial:
+     * @author Belal Khan
+     * @link https://www.simplifiedcoding.net/android-volley-tutorial-fetch-json/
+     */
+    private void loadArticles() {
+
+        // Make progress bar visible
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        // Create the request
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, JSON_URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                // Hide the progress bar
+                progressBar.setVisibility(View.GONE);
+
+                try {
+                    // Get the JSON object from the response
+                    JSONObject obj = new JSONObject(response);
+
+                    // Get the articles array from the JSON object
+                    JSONArray articles = obj.getJSONArray("articles");
+
+                    // Loop through the array
+                    for (int i = 0; i < articleQty; i++) {
+
+                        // Get the JSON object of this article
+                        JSONObject articleObject = articles.getJSONObject(i);
+
+                        // Get the data we want from the JSON object
+                        String title = articleObject.getString("title");
+                        //String url = articleObject.getString("url");
+                        //String imageUrl = articleObject.getString("urlToImage");
+                        String url = "google.com";
+                        String imageUrl = "google.com";
+
+                        // Create an Article object (our article class object, not the JSON object)
+                        Article thisArticle = new Article(title, url, imageUrl);
+
+                        // Add this article to the list that the RecyclerView will use
+                        articleList.add(thisArticle);
+                    }
+
+                    // Lookup the recyclerview in the activity layout
+                    RecyclerView rvArticles = findViewById(R.id.rv_latestNews);
+
+                    // Create adapter passing in the outlets
+                    ArticleAdapter articleAdapter = new ArticleAdapter(articleList, getApplicationContext());
+
+                    // Attach the adapter to the recyclerview to populate items
+                    rvArticles.setAdapter(articleAdapter);
+
+                    // Set layout manager to position the items
+                    rvArticles.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },
+
+        new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Display error in toast
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Create the request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // Add the string request to request queue
+        requestQueue.add(stringRequest);
     }
 
 
